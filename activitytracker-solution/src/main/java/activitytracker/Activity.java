e274c53021d03d761cd3ccea5c8ea4a1edc434f0
ipp203 <ipp203@freemail.hu>
2021-07-17 15:20:12 +0200

    ActivityTrackerJPA - 3. Entitások konfigurálása

--- activitytracker-solution/src/main/java/activitytracker/Activity.java
+++ activitytracker-solution/src/main/java/activitytracker/Activity.java
@@ -15,12 +15,14 @@ public class Activity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "start_time")
+    @Column(name = "start_time", nullable = false)
     private LocalDateTime startTime;
 
+    @Column(nullable = false, length = 200)
     private String description;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
     private Type type;
 
     public Activity(LocalDateTime startTime, String description, Type type) {
